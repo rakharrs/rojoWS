b0362885994014f5b0843d8b5d652aa91a5bd246
rakharrs <rodyox.yoann@gmail.com>
2023-11-21 13:51:01 +0300

    init

---
+++ src/main/java/com/etu1999/rojows/RojowsApplication.java
@@ -0,0 +1,13 @@
+package com.etu1999.rojows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RojowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RojowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etu1999/rojows/RojowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etu1999.rojows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RojowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
