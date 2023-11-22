39e916980f81aa1644a9e2b443d5b56cfe861e1d
rakharrs <rodyox.yoann@gmail.com>
2023-11-22 08:47:56 +0300

    resolve conflict

--- src/main/java/com/etu1999/rojows/service/FranchiseService.java
+++ src/main/java/com/etu1999/rojows/service/FranchiseService.java
@@ -5,15 +5,19 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.etu1999.rojows.domain.Franchise;
+import com.etu1999.rojows.domain.Game;
 import com.etu1999.rojows.domain.Player;
 import com.etu1999.rojows.repository.FranchiseRepository;
+import com.etu1999.rojows.repository.GameRepository;
 
 @Service
 public class FranchiseService {
     private final FranchiseRepository franchiseRepository;
+    private final GameRepository gameRepository;
 
-    public FranchiseService(final FranchiseRepository franchiseRepository) {
+    public FranchiseService(final FranchiseRepository franchiseRepository, final GameRepository gameRepository) {
         this.franchiseRepository = franchiseRepository;
+        this.gameRepository = gameRepository;
     }
 
     public List<Franchise> findAll(){
@@ -22,5 +26,15 @@ public class FranchiseService {
 
     public List<Player> getPlayers(String id){
         return franchiseRepository.findPlayerByFranchiseId(id);
+    }
+
+    
+
+    public List<Game> findHomeGameByFranchiseId(String id_franchise){
+        return gameRepository.findHomeGameByFranchiseId(id_franchise);
+    }
+
+    public List<Game> findOutsideGameByFranchiseId(String id_franchise){
+        return gameRepository.findOutsideGameByFranchiseId(id_franchise);
     }
 }
